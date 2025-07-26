a727f36eaf5b558d2fbff438434ddafd94f7f942
Ayush jain <ayushjain@Ayushs-MacBook-Air.local>
2025-07-27 04:34:12 +0530

    Removed turbopack bundler to run babel for testing with jest

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Counter App",
@@ -19,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         {children}
       </body>
     </html>
