951b98de7e3bb945fb239747e3f977bcb4a98480
Ayush jain <ayushjain@Ayushs-MacBook-Air.local>
2025-07-27 05:20:33 +0530

    Implemented env

--- src/app/components/counter.js
+++ src/app/components/counter.js
@@ -5,6 +5,10 @@ import { useState } from "react";
 export default function Counter() {
   const [count, setCount] = useState(0);
 
+  // Maximum and minimum count values from environment variables
+  const MAX_COUNT = parseInt(process.env.NEXT_PUBLIC_MAX_COUNT, 10) || 10;
+  const MIN_COUNT = parseInt(process.env.NEXT_PUBLIC_MIN_COUNT, 10) || 0;
+
   // Function to handle increment
   const handleIncrement = () => {
     setCount(count + 1);
@@ -21,9 +25,9 @@ export default function Counter() {
         <h1>Welcome to the Counter App</h1>
         <br />
         <div>
-          <button disabled={count === 10} onClick={handleIncrement}>+</button>
+          <button disabled={count === MAX_COUNT} onClick={handleIncrement}>+</button>
           <span style={{ margin: "0 10px" }}>Count: {count}</span>
-          <button disabled={count === 0} onClick={handleDecrement}>
+          <button disabled={count === MIN_COUNT} onClick={handleDecrement}>
             -
           </button>
         </div>
