1201e46615bec1d55fbcb6b6bfef100f98dcdd06
Ayush jain <ayushjain@Ayushs-MacBook-Air.local>
2025-07-27 03:37:00 +0530

    Added test cases for counter component

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+};
---
+++ src/app/components/counter.test.js
@@ -0,0 +1,44 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Counter from "./counter";
+
+describe("Counter Component", () => {
+  test("Render counter buttons", () => {
+    render(<Counter />);
+    expect(screen.getByText("+")).toBeInTheDocument();
+    expect(screen.getByText("-")).toBeInTheDocument();
+  });
+
+  test("Initial count to be 0", () => {
+    render(<Counter />);
+    expect(screen.getByText(/Count: 0/i)).toBeInTheDocument();
+  });
+
+  test("Increment button to increase the count", () => {
+    render(<Counter />);
+    fireEvent.click(screen.getByText("+"));
+    expect(screen.getByText(/Count: 1/i)).toBeInTheDocument();
+  });
+
+  test("Decrement button should decrease the count", () => {
+    render(<Counter />);
+    const incrementBtn = screen.getByText("+");
+    const decrementBtn = screen.getByText("-");
+    fireEvent.click(incrementBtn); // Count: 1
+    fireEvent.click(decrementBtn); // Count: 0
+    expect(screen.getByText(/Count: 0/i)).toBeInTheDocument();
+  });
+
+  test("Decrement button is disabled at count 0", () => {
+    render(<Counter />);
+    expect(screen.getByText("-")).toBeDisabled();
+  });
+
+  test("Decrement button is enabled at count greater than 0", () => {
+    render(<Counter />);
+    const incrementBtn = screen.getByText("+");
+    const decrementBtn = screen.getByText("-");
+    fireEvent.click(incrementBtn);
+    expect(decrementBtn).not.toBeDisabled();
+  });
+});
