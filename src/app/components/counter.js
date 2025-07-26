39b671d79e3230e6f681d3aa62b3270012169f8b
Ayush jain <ayushjain@Ayushs-MacBook-Air.local>
2025-07-27 01:44:03 +0530

    The counter component

---
+++ src/app/components/counter.js
@@ -0,0 +1,33 @@
+"use client"; 
+
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  // Function to handle increment
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  // Function to handle decrement
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <center>
+        <br />
+        <h1>Welcome to the Counter App</h1>
+        <br />
+        <div>
+          <button onClick={handleIncrement}>+</button>
+          <span style={{ margin: "0 10px" }}>Count: {count}</span>
+          <button disabled={count === 0} onClick={handleDecrement}>
+            -
+          </button>
+        </div>
+      </center>
+    </div>
+  );
+}
